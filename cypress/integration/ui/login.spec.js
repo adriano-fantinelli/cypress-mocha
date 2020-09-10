1fcf9a819edefa0e89779c91b65ec74afacefc98
Adriano Fantinelli <adrianofa2013@gmail.com>
2020-09-10 17:14:21 -0300

    Modificando UI

--- cypress/integration/ui/login.spec.js
+++ cypress/integration/ui/login.spec.js
@@ -1,15 +1,29 @@
 /// <reference types="cypress" />
-import LoginPage from "../../support/pages/login"
+import LoginPage from "../../support/pages/login";
 
+// Traduzir para o inglês
 context('Fazer login', () => {
-
+    let dadosLogin
     beforeEach(() => {
+        cy.fixture('login.json').then( d => {
+            dadosLogin = d
+        })
         cy.visit("/")
     })
 
     it('Deve realizar um login com sucesso', () => {
-        LoginPage.preencherLogin("novoemailteste@gmail.com", "teste123")
+        LoginPage.preencherLogin(dadosLogin.loginSucesso.email, dadosLogin.loginSucesso.senha)
         cy.get('.account > span').should('have.text', 'Automação Teste')
+    })
+
+    it('Deve realizar um login sem sucesso com um email inválido', () => {
+        LoginPage.preencherLogin(dadosLogin.loginSemSucessoEmailInvalido.email, dadosLogin.loginSemSucessoEmailInvalido.senha)
+        cy.get('ol > li').should('have.text', 'Authentication failed.')
+    })
+
+    it('Deve realizar um login sem sucesso com um email valido', () => {
+        LoginPage.preencherLogin(dadosLogin.loginSemSucessoSenhaInvalida.email, dadosLogin.loginSemSucessoSenhaInvalida.senha)
+        cy.get('ol > li').should('have.text', 'Authentication failed.')
     })
 
 })
