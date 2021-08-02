f61bd608dc93a938f8d47cea514aa15536562f09
ADRIANO FANTINELLI <adriano.fantinelli-ext@viavarejo.com.br>
2021-08-02 14:50:53 -0300

    Trying to execute on actions

--- cypress/integration/api/activities.spec.js
+++ cypress/integration/api/activities.spec.js
@@ -3,43 +3,43 @@
 import activitySchema from '../../contracts/activity.contract'
 import activitiesSchema from '../../contracts/activities.contract'
 
-context('Activities', () => {
-    beforeEach(() => {
-      cy.fixture('createActivity').as('createActivity')
-      cy.fixture('editActivity').as('editActivity')
-    })
+context('Activities', { tags: ['@regressivo', '@activities'] }, () => {
+  beforeEach(() => {
+    cy.fixture('createActivity').as('createActivity')
+    cy.fixture('editActivity').as('editActivity')
+  })
 
-    it('Search for all activities', function() {
-        cy.getActivitiesApi().then((response) => {
-          expect(response.status).to.eq(200)
-          activitiesSchema.validateAsync(response.body)
-        })
+  it('Search for all activities', { tags: ['@smoke'] }, function () {
+    cy.getActivitiesApi().then((response) => {
+      expect(response.status).to.eq(200)
+      activitiesSchema.validateAsync(response.body)
     })
+  })
 
-    it('Create an activity', function() {
-      cy.postActivityApi(this.createActivity.ID, this.createActivity.Title, this.createActivity.DueDate, this.createActivity.Completed).then((response) => {
-        expect(response.status).to.eq(200)
-        activitySchema.validateAsync(response.body)
-      })
+  it('Create an activity', function () {
+    cy.postActivityApi(this.createActivity.ID, this.createActivity.Title, this.createActivity.DueDate, this.createActivity.Completed).then((response) => {
+      expect(response.status).to.eq(200)
+      activitySchema.validateAsync(response.body)
     })
+  })
 
-    it('Delete an activity', function() {
-        cy.deleteActivityApi(30).then((response) => {
-            expect(response.status).to.eq(200)
-        })
+  it('Delete an activity', function () {
+    cy.deleteActivityApi(30).then((response) => {
+      expect(response.status).to.eq(200)
     })
+  })
 
-    it('Search for an specific activity', function() {
-        cy.getActivityApi(30).then((response) => {
-            expect(response.status).to.eq(200)
-            activitySchema.validateAsync(response.body)
-        })
+  it('Search for an specific activity', function () {
+    cy.getActivityApi(30).then((response) => {
+      expect(response.status).to.eq(200)
+      activitySchema.validateAsync(response.body)
     })
+  })
 
-    it('Edit an activity', function() {
-        cy.putActivityApi(this.editActivity.ID, this.editActivity.Title, this.editActivity.DueDate, this.editActivity.Completed).then((response) => {
-            expect(response.status).to.eq(200)
-            activitySchema.validateAsync(response.body)
-        })     
+  it('Edit an activity', function () {
+    cy.putActivityApi(this.editActivity.ID, this.editActivity.Title, this.editActivity.DueDate, this.editActivity.Completed).then((response) => {
+      expect(response.status).to.eq(200)
+      activitySchema.validateAsync(response.body)
     })
+  })
 })
--- cypress/integration/ui/login.spec.js
+++ cypress/integration/ui/login.spec.js
@@ -1,24 +1,24 @@
 /// <reference types="cypress" />
 import LoginPage from "../../support/pages/login";
 
-context('Login', () => {
-    beforeEach(() => {
-        cy.fixture('login').as('login')
-        cy.visit("/")
-    })
+context('Login', { tags: ['@regressivo', '@login'] }, () => {
+  beforeEach(() => {
+    cy.fixture('login').as('login')
+    cy.visit("/")
+  })
 
-    it('Must login successfully', function() {
-        LoginPage.login(this.login.loginSuccess.email, this.login.loginSuccess.password)
-        cy.get('.account > span').should('have.text', 'Novo email')
-    })
+  it('Must login successfully', { tags: ['@smoke'] }, function () {
+    LoginPage.login(this.login.loginSuccess.email, this.login.loginSuccess.password)
+    cy.get('.account > span').should('have.text', 'Novo email')
+  })
 
-    it('Must login without success and with an incorrect email', function() {
-        LoginPage.login(this.login.loginWithoutSuccessInvalidEmail.email, this.login.loginWithoutSuccessInvalidEmail.password)
-        cy.get('ol > li').should('have.text', 'Authentication failed.')
-    })
+  it('Must login without success and with an incorrect email', function () {
+    LoginPage.login(this.login.loginWithoutSuccessInvalidEmail.email, this.login.loginWithoutSuccessInvalidEmail.password)
+    cy.get('ol > li').should('have.text', 'Authentication failed.')
+  })
 
-    it('Must login without sucess and with an incorrect password', function() {
-        LoginPage.login(this.login.loginWithoutSuccessInvalidPassword.email, this.login.loginWithoutSuccessInvalidPassword.password)
-        cy.get('ol > li').should('have.text', 'Authentication failed.')
-    })
+  it('Must login without sucess and with an incorrect password', function () {
+    LoginPage.login(this.login.loginWithoutSuccessInvalidPassword.email, this.login.loginWithoutSuccessInvalidPassword.password)
+    cy.get('ol > li').should('have.text', 'Authentication failed.')
+  })
 })
