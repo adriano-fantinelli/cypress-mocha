2477cad8a3571b423675a82aa2dc8a8ec1df39da
Adriano Fantinelli <adrianofa2013@gmail.com>
2021-08-30 21:54:34 -0300

    Isolating scenarios.

--- cypress/integration/api/activities.spec.js
+++ cypress/integration/api/activities.spec.js
@@ -12,34 +12,38 @@ context('Activities', { tags: ['@regression', '@activities'] }, () => {
   it('Search for all activities', { tags: ['@smoke'] }, function () {
     cy.getActivitiesApi().then((response) => {
       expect(response.status).to.eq(200)
-      activitiesSchema.validateAsync(response.body)
-    })
-  })
-
-  it('Create an activity', function () {
-    cy.postActivityApi(this.createActivity.ID, this.createActivity.Title, this.createActivity.DueDate, this.createActivity.Completed).then((response) => {
-      expect(response.status).to.eq(200)
-      activitySchema.validateAsync(response.body)
-    })
-  })
-
-  it('Delete an activity', function () {
-    cy.deleteActivityApi(30).then((response) => {
-      expect(response.status).to.eq(200)
+      activitiesSchema.valIdateAsync(response.body)
     })
   })
 
   it('Search for an specific activity', function () {
     cy.getActivityApi(30).then((response) => {
       expect(response.status).to.eq(200)
-      activitySchema.validateAsync(response.body)
+      activitySchema.valIdateAsync(response.body)
+    })
+  })
+
+  it('Create an activity', function () {
+    cy.postActivityApi(this.createActivity.Id, this.createActivity.Title, this.createActivity.DueDate, this.createActivity.Completed).then((response) => {
+      expect(response.status).to.eq(200)
+      activitySchema.valIdateAsync(response.body)
+    })
+    cy.deleteActivityApi(this.createActivity.Id)
+  })
+
+  it('Delete an activity', function () {
+    cy.postActivityApi(this.createActivity.Id, this.createActivity.Title, this.createActivity.DueDate, this.createActivity.Completed)
+    cy.deleteActivityApi(this.createActivity.Id).then((response) => {
+      expect(response.status).to.eq(200)
     })
   })
 
   it('Edit an activity', function () {
-    cy.putActivityApi(this.editActivity.ID, this.editActivity.Title, this.editActivity.DueDate, this.editActivity.Completed).then((response) => {
+    cy.postActivityApi(this.createActivity.Id, this.createActivity.Title, this.createActivity.DueDate, this.createActivity.Completed)
+    cy.putActivityApi(this.editActivity.Id, this.editActivity.Title, this.editActivity.DueDate, this.editActivity.Completed).then((response) => {
       expect(response.status).to.eq(200)
-      activitySchema.validateAsync(response.body)
+      activitySchema.valIdateAsync(response.body)
     })
+    cy.deleteActivityApi(this.createActivity.Id)
   })
 })
